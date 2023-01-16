1f8e4c7006c5c96c43dfda376798ef50efaf657a
username=nam157 <nguyenhoainamhuongkhe2@gmail.com>
2023-01-16 21:44:27 +0700

    init

---
+++ envs.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+
+class Envs:
+    def __init__(self) -> None:
+        pass
+    def create_log(self):
+        pass
+    def reset(self):
+        pass
+    def _next_observation(self):
+        pass
+    def step(self):
+        pass
+    def reader(self):
+        pass
+    def get_gaes(self):
+        pass
+    def replay(self):
+        pass
+    def act(self):
+        pass
+    def save(self):
+        pass
+    def load(self):
+        pass
